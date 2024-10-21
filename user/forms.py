3d56d6e52107e8e64d4d9a0a3111cf11243b05df
Mikhail Tsapkov <matsapkov@gmail.com>
2024-10-21 03:00:38 +0300

    Начало работы с формами. Надо будет сделать валидацию

---
+++ user/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from user.models import User
+from django import forms
+
+
+class UserLoginForm(AuthenticationForm):
+    email = forms.EmailField(widget=forms.TextInput(attrs={
+        'class': 'email',
+        'id': 'email'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'password',
+        'id': 'password'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+
--- user/views.py
+++ user/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import render, HttpResponseRedirect, redirect
+from django.contrib import auth
+from django.urls import reverse
+from user.models import User
+from user.forms import UserLoginForm
 
 # Create your views here.
 
 
 def login(request):
-    context = {'title': 'Вход'}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        print('--------------------------------------------------')
+        if form.is_valid():
+            print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            # email = request.POST['email']
+            # password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'user/login.html', context)
+
+
 
 
 def registration(request):
