4f82524a7d2757f31f6671c6903ccf2f8001b707
mikhailtsapkov <matsapkov@gmail.com>
2024-10-22 16:27:23 +0300

    Теперь при регистрации пользователя поле email будет обязательным для заполнения

--- user/models.py
+++ user/models.py
@@ -6,6 +6,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-
+    email = models.EmailField("email address", blank=False, unique=True, null=False)
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
 
